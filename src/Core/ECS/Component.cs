32a9feb7ae49ef967b1701de0b87f0ffca289a85
cwahlfeldt <chriswahlfeldt@gmail.com>
2024-11-08 22:31:10 -0600

    explore ecs

--- src/Core/ECS/Component.cs
+++ src/Core/ECS/Component.cs
@@ -4,6 +4,11 @@ namespace HexTactics.Core.ECS
 {
     public abstract partial class Component : Node
     {
-        public new Entity Owner => GetParent() as Entity;
+        public new Entity Owner => GetParent<Entity>();
+
+        protected Component() { }
+
+        // Create clean copy of component
+        public abstract Component Clone();
     }
 }
--- src/Core/ECS/Entity.cs
+++ src/Core/ECS/Entity.cs
@@ -1,29 +1,45 @@
 using Godot;
-using System;
-using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace HexTactics.Core.ECS
 {
     public abstract partial class Entity : Node3D
     {
-        protected Dictionary<Type, Component> Components = new();
+        // Immutable list of components
+        private ImmutableList<Component> _components = ImmutableList<Component>.Empty;
+
+        public Entity()
+        {
+            _components = ImmutableList<Component>.Empty;
+        }
 
         public T AddComponent<T>() where T : Component, new()
         {
             var component = new T();
             AddChild(component);
-            Components[typeof(T)] = component;
+            _components = _components.Add(component);
             return component;
         }
 
         public T GetComponent<T>() where T : Component
         {
-            return Components.GetValueOrDefault(typeof(T)) as T;
+            return _components.Find(c => c is T) as T;
         }
 
         public bool HasComponent<T>() where T : Component
         {
-            return Components.ContainsKey(typeof(T));
+            return _components.Any(c => c is T);
+        }
+
+        public void RemoveComponent<T>() where T : Component
+        {
+            var component = GetComponent<T>();
+            if (component != null)
+            {
+                _components = _components.Remove(component);
+                component.QueueFree();
+            }
         }
     }
 }
